0f662c5de2d1d5e68a530204f94e71cf29bd2629
Acrobaticbag3 <kevintom214@gmail.com>
2026-01-11 22:39:52 +0100

    SUT setup compleate, will look for syntax compliance later

--- test-configs/customTest/multimediaTest.cpp
+++ test-configs/customTest/multimediaTest.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 namespace waybar::modules {
+  // === === === Semi-mocekd pulseaudio module === === ===
   class Pulseaudio {
     public:
       Pulseaudio(const std::string& id, const Json::Value& config) : id_(id), config_(config) {}
@@ -12,6 +13,17 @@ namespace waybar::modules {
       virtual void update() {
         // pretend communication
       }
-  }
+
+    protected:
+      virtual std::string format() const {
+        if (muted_) return "[muted]";
+        return std::to_string(volume_) + "%";
+      }
+
+      std::string id_;
+      Json::Value config_;
+      uint32_t volume_{0};
+      bool muted_{false};
+  };
 }
 
